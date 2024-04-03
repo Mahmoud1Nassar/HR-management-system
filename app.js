c8f278c5183a6b2d4e0ef97536fb4bc837f53a13
Mahmoud1Nassar <mahmoud.nassar2001@outlook.com>
2024-04-03 21:35:01 +0300

    made table on the accounting page

--- app.js
+++ app.js
@@ -1,11 +1,60 @@
-function Employee(employeeID, fullName, department, level, imageUrl) {
-    this.employeeID = employeeID;
-    this.fullName = fullName;
-    this.department = department;
-    this.level = level;
-    this.imageUrl = imageUrl;
-    this.salary = calculateSalary(level);
-    this.netSalary = calculateNetSalary(this.salary);
+class Employee {
+    constructor(employeeID, fullName, department, level, imageUrl) {
+        this.employeeID = employeeID;
+        this.fullName = fullName;
+        this.department = department;
+        this.level = level;
+        this.imageUrl = imageUrl;
+        this.salary = calculateSalary(level);
+        this.netSalary = calculateNetSalary(this.salary);
+    }
+         render() {
+        //Making Departments
+        const administration = document.getElementById("Administration");
+        const finance = document.getElementById("Finance");
+        const marketing = document.getElementById("Marketing");
+        const development = document.getElementById("Development");
+        //Making Divs for each employee
+        const employeeDiv = document.createElement("div");
+        employeeDiv.classList.add("employee");
+        employeeDiv.innerHTML = `
+        <h2>${this.fullName}</h2>
+        <img src="${this.imageUrl}" alt="${this.fullName}">
+        <p>ID: ${this.employeeID}</p>
+        <p>Department: ${this.department}</p>
+        <p>Level: ${this.level}</p>
+        <p>Net-Salary: ${this.netSalary.toFixed(2)}</p>`;
+        //rendering dives inside the situational department
+        if (this.department == 'Administration') {
+            administration.appendChild(employeeDiv);
+        }
+        else if (this.department == 'Finance') {
+            finance.appendChild(employeeDiv);
+        }
+        else if (this.department == 'Marketing') {
+            marketing.appendChild(employeeDiv);
+        }
+        else if (this.department == 'Development') {
+            development.appendChild(employeeDiv);
+        }
+    }
+}
+let click = 0;
+function saveData(employee) {
+    click++;
+    localStorage.setItem('employee_' + click, JSON.stringify(employee));
+}
+
+function getData() {
+    const employees = [];
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        if (key.startsWith('employee_')) {
+            const employeeData = JSON.parse(localStorage.getItem(key));
+            employees.push(employeeData);
+        }
+    }
+    return employees;
 }
 
 function calculateSalary(level) {
@@ -37,50 +86,10 @@ const employees = [
     { employeeID: 1006, fullName: "Hadi Ahmad", department: "Finance", level: "Mid-Senior", imageUrl: "Images/Hadi Ahmad.jpg" }
 ]
 
-
-
-Employee.prototype.render = function () {
-    //Making Departments
-    const administration = document.getElementById("Administration");
-    const finance = document.getElementById("Finance");
-    const marketing = document.getElementById("Marketing");
-    const development = document.getElementById("Development");
-    //Making Divs for each employee
-    const employeeDiv = document.createElement("div");
-    employeeDiv.classList.add("employee");
-    employeeDiv.innerHTML = `
-        <h2>${this.fullName}</h2>
-        <img src="${this.imageUrl}" alt="${this.fullName}">
-        <p>ID: ${this.employeeID}</p>
-        <p>Department: ${this.department}</p>
-        <p>Level: ${this.level}</p>
-        <p>Net-Salary: ${this.netSalary.toFixed(2)}</p>`;
-    //rendering dives inside the situational department
-    if (this.department == 'Administration') {
-        administration.appendChild(employeeDiv);
-    }
-    else if (this.department == 'Finance') {
-        finance.appendChild(employeeDiv);
-    }
-    else if (this.department == 'Marketing') {
-        marketing.appendChild(employeeDiv);
-    }
-    else if (this.department == 'Development') {
-        development.appendChild(employeeDiv);
-    }
-
-};
-
 employees.forEach(employeeData => {
     const employee = new Employee(employeeData.employeeID, employeeData.fullName, employeeData.department, employeeData.level, employeeData.imageUrl);
-    employee.render();
+    saveData(employee);
 });
-
-
-
-
-
-
 
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('createEmployeeForm').classList.add('hidden');
@@ -90,15 +99,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
         form.classList.toggle('hidden');
     });
 
-
     document.getElementById('createEmployeeForm').addEventListener('submit', function (event) {
         event.preventDefault();
-
         const fullName = document.getElementById('fullName').value;
         const department = document.getElementById('department').value;
         const level = document.getElementById('level').value;
         const imageUrl = document.getElementById('imageUrl').value
         let emp = new Employee(generateID(), fullName, department, level, imageUrl);
+        saveData(emp);
         emp.render();
     })
+});
+
+const storedEmployees = getData();
+storedEmployees.forEach(employeeData => {
+    const employee = new Employee(employeeData.employeeID, employeeData.fullName, employeeData.department, employeeData.level, employeeData.imageUrl);
+    employee.render();
 });
